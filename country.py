d0ee8a1f175e52e0c0adf2d35e66894beb1d956a
S2748150 <s2748150@lett-edu-176-225.let.rug.nl>
2015-02-20 12:32:03 +0100

    modified country.py

--- country.py
+++ country.py
@@ -1,12 +1,30 @@
 import sys
+from PyQt4 import QtCore, QtGui
+from random import randrange
+
 
 class Country:
 	
 	def __init__(self,country):
 		self.country = country
-		
+		flag=Flagcolor()
+		self.color=flag.getcolor()
 	def __str__(self):
 		return "Hello from {0}".format(self.country)
+	
+		
+class Flagcolor(QtGui.QColor):
+	def __init__(self):
+		self.color=QtGui.QColor()
+		
+	def getcolor(self):
+		
+		self.color=QtGui.QColor()
+		self.color.setRed(randrange(0,256))
+		self.color.setGreen(randrange(0,256))
+		self.color.setBlue(randrange(0,256))
+		return self.color
+		
 		
 def main():
 	countryname = Country("Holland")
