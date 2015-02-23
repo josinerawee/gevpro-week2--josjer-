8bccf9c652dc4a96813cf803849ba8c76f8990e4
S2748150 <s2748150@lett-edu-176-225.let.rug.nl>
2015-02-23 11:38:26 +0100

    opdracht afgemaakt

--- country.py
+++ country.py
@@ -3,28 +3,21 @@ from PyQt4 import QtCore, QtGui
 from random import randrange
 
 
-class Country:
-	
+class Country(QtGui.QColor):
+	"""Genereert een country object en geeft een random kleur"""
 	def __init__(self,country):
 		self.country = country
-		flag=Flagcolor()
-		self.color=flag.getcolor()
+		
 	def __str__(self):
 		return "Hello from {0}".format(self.country)
-	
-		
-class Flagcolor(QtGui.QColor):
-	def __init__(self):
-		self.color=QtGui.QColor()
 		
 	def getcolor(self):
 		
-		self.color=QtGui.QColor()
+		self.color=QtGui.QColor(0,0,0)
 		self.color.setRed(randrange(0,256))
 		self.color.setGreen(randrange(0,256))
 		self.color.setBlue(randrange(0,256))
 		return self.color
-		
 		
 def main():
 	countryname = Country("Holland")
